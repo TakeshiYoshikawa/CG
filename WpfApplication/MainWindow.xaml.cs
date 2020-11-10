3a24c08283f4c194e971829f901ae925617aaa9d
TakeshiYoshikawa <takeshi-y@hotmail.com>
2020-11-10 01:41:31 -0300

    Change from DynamicGrid to UniformGrid

--- WpfApplication/MainWindow.xaml.cs
+++ WpfApplication/MainWindow.xaml.cs
@@ -16,24 +16,65 @@ using WpfApplication.ViewModels;
 
 namespace WpfApplication
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-			var dynamicGridViewModel = new DynamicGridViewModel
-			{
-				GridWidth = 25,
-				GridHeight = 15,
-				BorderColor = Colors.Blue,
-				StartColor = Colors.Azure,
-				FinishColor = Colors.CornflowerBlue,
-			};
+    public partial class MainWindow : Window
+    {
+        List<MatrixElement> _board;
+        public MainWindow()
+        {
+            InitializeComponent();
+            int rows = 20;
+            int columns = 20;
 
-			DataContext = dynamicGridViewModel;
-		}
-	}
+            _board = new List<MatrixElement>();
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < columns; c++)
+                {
+                    _board.Add(new MatrixElement(r, c) { Color = "White" });
+                }
+            }
+            Board.ItemsSource = _board;
+        }
+
+        private void CellClick(object sender, MouseButtonEventArgs e)
+        {
+            var border = (Border)sender;
+            var point = (MatrixElement)border.Tag;
+            point.Color = "#00BFFF";
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Test", "Test");
+        }
+    }
+
+    public class MatrixElement
+    {
+        private string _color;
+
+        public MatrixElement(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public string Color
+        {
+            get { return _color; }
+            set
+            {
+                _color = value;
+                if (ColorChanged != null)
+                {
+                    ColorChanged(this, EventArgs.Empty);
+                    MessageBox.Show("[" + this.X.ToString() + "]" + "[" + this.Y.ToString() + "]");
+                }
+            }
+        }
+        public event EventHandler ColorChanged;
+    }
 }
