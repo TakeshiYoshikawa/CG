e8632d69a2f5d4929821d89b1ad13853718177ea
TakeshiYoshikawa <takeshi-y@hotmail.com>
2020-11-25 15:20:42 -0300

    Added semi-functional Bresenham method.

--- WpfApplication/MainWindow.xaml.cs
+++ WpfApplication/MainWindow.xaml.cs
@@ -18,19 +18,22 @@ namespace WpfApplication
 {
     public partial class MainWindow : Window
     {
-        List<MatrixElement> _board;
+        private int rows = 20;
+        private int columns = 20;
+        public List<Pixel> _board;
+        public List<int> coordinates;
+
         public MainWindow()
         {
             InitializeComponent();
-            int rows = 20;
-            int columns = 20;
 
-            _board = new List<MatrixElement>();
+            _board = new List<Pixel>();
+            coordinates = new List<int>();
             for (int r = 0; r < rows; r++)
             {
                 for (int c = 0; c < columns; c++)
                 {
-                    _board.Add(new MatrixElement(r, c) { Color = "White" });
+                    _board.Add(new Pixel(r, c) { Color = "White" });
                 }
             }
             Board.ItemsSource = _board;
@@ -39,21 +42,40 @@ namespace WpfApplication
         private void CellClick(object sender, MouseButtonEventArgs e)
         {
             var border = (Border)sender;
-            var point = (MatrixElement)border.Tag;
-            point.Color = "#00BFFF";
+            var point = (Pixel)border.Tag;
+            point.Color = "Red";
+
+            coordinates.Add(point.X);
+            coordinates.Add(point.Y);
+            //MessageBox.Show("[" + point.X.ToString() + "][" + point.Y.ToString() + "]");
         }
 
-        private void ButtonClick(object sender, RoutedEventArgs e)
+        public int GetIndex(int x, int y)
         {
-            MessageBox.Show("Test", "Test");
+            return y + (x * rows);
+        }
+        
+        private void Line(object sender, RoutedEventArgs e)
+        {
+            var bresenham = new Bresenham();
+            bresenham.Algorithm(coordinates[0], coordinates[1], coordinates[2], coordinates[3]);
+        }
+
+        public void RefreshUI(object sender, RoutedEventArgs e)
+        {
+            coordinates.Clear();
+            foreach (var i in _board)
+            {
+                    i.Color = "White";
+            }
         }
     }
 
-    public class MatrixElement
+    public class Pixel
     {
         private string _color;
 
-        public MatrixElement(int x, int y)
+        public Pixel(int x, int y)
         {
             X = x;
             Y = y;
@@ -71,10 +93,156 @@ namespace WpfApplication
                 if (ColorChanged != null)
                 {
                     ColorChanged(this, EventArgs.Empty);
-                    MessageBox.Show("[" + this.X.ToString() + "]" + "[" + this.Y.ToString() + "]");
                 }
             }
         }
         public event EventHandler ColorChanged;
+    }
+
+    public class Point
+    {
+        public int X;
+        public int Y;
+
+        public void SetCoordinates(int a, int b)
+        {
+            X = a;
+            Y = b;
+        }
+    }
+    public class Bresenham
+    {
+        List<int> xn, yn;
+        Point p1, p2;
+        double m;
+        bool tradeX, tradeY, tradeXY;
+
+        public Bresenham()
+        {
+            p1 = new Point();
+            p2 = new Point();
+            xn = new List<int>();
+            yn = new List<int>();
+
+            m = 0;
+            tradeX = false;
+            tradeY = false;
+            tradeXY = false;
+        }
+
+        public void Algorithm(int x1, int y1, int x2, int y2)
+        {
+            p1.SetCoordinates(x1, y1);
+            p2.SetCoordinates(x2, y2);
+
+            Reflect();
+
+            int x = p1.X;
+            int y = p1.Y;
+
+            double dx = Math.Abs(p2.X - p1.X);
+            double dy = Math.Abs(p2.Y - p1.Y);
+
+            m = dy / dx;
+            double e = m - 1.0 / 2.0;
+
+            SetPixels(x, y);
+
+            while(x < p2.X)
+            {
+                if(e > 0)
+                {
+                    y++;
+                    e--;
+                }
+                x++;
+                e += m;
+                SetPixels(x, y);
+            }
+            Deflect();
+            DrawPoints();
+        }
+        public void Reflect()
+        {
+            int dx = (p2.X - p1.X);
+            int dy = (p2.Y - p1.Y);
+
+            int m = dy / dx;
+
+            if (m > 1 || m < -1)
+            {
+                ExtensionMethods.Swap(ref p1.X, ref p1.Y);
+                ExtensionMethods.Swap(ref p2.X, ref p2.Y);
+                tradeXY = true;
+            }
+            if(p1.X > p2.X)
+            {
+                p1.X = -p1.X;
+                p2.X = -p2.X;
+                tradeX = true;
+            }
+            if(p1.Y > p2.Y)
+            {
+                p1.Y = -p1.Y;
+                p2.Y = -p2.Y;
+                tradeY = true;
+            }
+        }
+        public void Deflect()
+        {
+            if (tradeY == true)
+            {
+                for (int i = 0; i < yn.Count(); i++)
+                {
+                    yn[i] = -yn[i];
+                }
+            }
+            if (tradeX == true)
+            {
+                for (int i = 0; i < xn.Count(); i++)
+                {                    
+                    xn[i] = -xn[i];
+                }
+            }
+            if (tradeXY == true)
+            {
+                for (int i = 0; i < xn.Count(); i++)
+                {
+                    ExtensionMethods.Swap(xn, yn, i);
+                }
+            }
+        }
+        public void SetPixels(int x, int y)
+        {
+            xn.Add(x);
+            yn.Add(y);
+        }
+
+        public void DrawPoints()
+        {
+            var windows = (MainWindow)Application.Current.MainWindow;
+            for(int i = 0; i < xn.Count(); i++)
+            {
+                int index = windows.GetIndex(xn[i], yn[i]);
+                //MessageBox.Show("[" + xn[i].ToString() + "][" + yn[i].ToString() + "] ; Index = " + windows.GetIndex(xn[i], yn[i]).ToString());
+                windows._board[index].Color = "Red";
+            }
+        }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+        public static void Swap<T>(this List<T> list1, List<T> list2, int index)
+        {
+            T temp = list1[index];
+            list1[index] = list2[index];
+            list2[index] = temp;
+        }
     }
 }
