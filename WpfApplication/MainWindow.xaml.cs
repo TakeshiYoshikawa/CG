aa04d363e1e1bb9580a8ed370dd028f95ba45451
TakeshiYoshikawa <takeshi-y@hotmail.com>
2020-12-08 02:52:55 -0300

    Add Cohen Sutherland algorithm

--- WpfApplication/MainWindow.xaml.cs
+++ WpfApplication/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Drawing;
 
 namespace WpfApplication
 {
@@ -57,7 +58,7 @@ namespace WpfApplication
         public void Line(object sender, RoutedEventArgs e)
         {
             var bresenham = new Bresenham();
-            bresenham.Algorithm(coordinates[0], coordinates[1]);
+            bresenham.Algorithm(coordinates[0], coordinates[1], "Red");
         }
 
         public void Circle(object sender, RoutedEventArgs e)
@@ -75,7 +76,7 @@ namespace WpfApplication
         public void Polyline(object sender, RoutedEventArgs e)
         {
             var polylines = new Polyline();
-            polylines.Algorithm(coordinates);
+            polylines.Algorithm(coordinates, "Red");
         }
 
         public void Fill(object sender, RoutedEventArgs e)
@@ -86,6 +87,24 @@ namespace WpfApplication
         public void Scanline(object sender, RoutedEventArgs e)
         {
             new Scanline().Sweep(coordinates);
+        }
+
+        public void TrimSubwindows(object sender, RoutedEventArgs e)
+        {
+            List<Point> trimBorder = new List<Point>() {
+                new Point(5,5),
+                new Point(5,25),
+                new Point(25,25),
+                new Point(25,5)
+            };
+            new Polyline().Algorithm(trimBorder, "Green");
+        }
+
+        public void LineTrim(object sender, RoutedEventArgs e)
+        {
+            var rect = new RectangleF(5, 5, 20, 20);
+            var newLine = CohenSutherland.ClipSegment(rect, coordinates[0], coordinates[1]);
+            new Bresenham().Algorithm(newLine.Item1, newLine.Item2, "Red");
         }
 
         public void Translation(object sender, RoutedEventArgs e)
@@ -111,6 +130,127 @@ namespace WpfApplication
             coordinates.Clear();
             foreach (var i in _board)
                 i.Color = "White";
+        }
+    }
+
+    public static class CohenSutherland
+    {
+
+        [Flags]
+        enum OutCode
+        {
+            Inside = 0,
+            Left = 1,
+            Right = 2,
+            Bottom = 4,
+            Top = 8
+        }
+
+        private static OutCode ComputeOutCode(float x, float y, RectangleF r)
+        {
+            var code = OutCode.Inside;
+
+            if (x < r.Left) code |= OutCode.Left;
+            if (x > r.Right) code |= OutCode.Right;
+            if (y < r.Top) code |= OutCode.Top;
+            if (y > r.Bottom) code |= OutCode.Bottom;
+
+            return code;
+        }
+        private static OutCode ComputeOutCode(Point p, RectangleF r) { return ComputeOutCode(p.X, p.Y, r); }
+
+        public static Tuple<Point, Point> ClipSegment(RectangleF r, Point p1, Point p2)
+        {
+            // classify the endpoints of the line
+            var outCodeP1 = ComputeOutCode(p1, r);
+            var outCodeP2 = ComputeOutCode(p2, r);
+            var accept = false;
+
+            while (true)
+            { // should only iterate twice, at most
+                // Case 1:
+                // both endpoints are within the clipping region
+                if ((outCodeP1 | outCodeP2) == OutCode.Inside)
+                {
+                    accept = true;
+                    break;
+                }
+
+                // Case 2:
+                // both endpoints share an excluded region, impossible for a line between them to be within the clipping region
+                if ((outCodeP1 & outCodeP2) != 0)
+                {
+                    break;
+                }
+
+                // Case 3:
+                // The endpoints are in different regions, and the segment is partially within the clipping rectangle
+
+                // Select one of the endpoints outside the clipping rectangle
+                var outCode = outCodeP1 != OutCode.Inside ? outCodeP1 : outCodeP2;
+
+                // calculate the intersection of the line with the clipping rectangle using parametric line equations
+                var p = CalculateIntersection(r, p1, p2, outCode);
+
+                // update the point after clipping and recaluculate outcode
+                if (outCode == outCodeP1)
+                {
+                    p1 = p;
+                    outCodeP1 = ComputeOutCode(p1, r);
+                }
+                else
+                {
+                    p2 = p;
+                    outCodeP2 = ComputeOutCode(p2, r);
+                }
+            }
+            // if clipping area contained a portion of the line
+            if (accept)
+            {
+                return new Tuple<Point, Point>(p1, p2);
+            }
+
+            // the line did not intersect the clipping area
+            return null;
+        }
+
+        private static Point CalculateIntersection(RectangleF r, Point p1, Point p2, OutCode clipTo)
+        {
+            var dx = (p2.X - p1.X);
+            var dy = (p2.Y - p1.Y);
+
+            var slopeY = dx / dy; // slope to use for possibly-vertical lines
+            var slopeX = dy / dx; // slope to use for possibly-horizontal lines
+
+            if (clipTo.HasFlag(OutCode.Top))
+            {
+                return new Point(
+                    (int)(p1.X + slopeY * (r.Top - p1.Y)),
+                    (int)(r.Top)
+                    );
+            }
+            if (clipTo.HasFlag(OutCode.Bottom))
+            {
+                return new Point(
+                    (int)(p1.X + slopeY * (r.Bottom - p1.Y)),
+                    (int)(r.Bottom)
+                    );
+            }
+            if (clipTo.HasFlag(OutCode.Right))
+            {
+                return new Point(
+                    (int)(r.Right),
+                    (int)(p1.Y + slopeX * (r.Right - p1.X))
+                    );
+            }
+            if (clipTo.HasFlag(OutCode.Left))
+            {
+                return new Point(
+                    (int)(r.Left),
+                    (int)(p1.Y + slopeX * (r.Left - p1.X))
+                    );
+            }
+            throw new ArgumentOutOfRangeException("clipTo = " + clipTo);
         }
     }
 
@@ -296,7 +436,7 @@ namespace WpfApplication
                 rotatedCoordinates.Add(ApplyRotationMatrix(p, matrix));
             }
             var figure = new Polyline();
-            figure.Algorithm(rotatedCoordinates);
+            figure.Algorithm(rotatedCoordinates, "Red");
         }
 
         ~Rotation() { }
@@ -353,7 +493,7 @@ namespace WpfApplication
         public void Draw()
         {
             var figure = new Polyline();
-            figure.Algorithm(GetTranslatedPoints());
+            figure.Algorithm(GetTranslatedPoints(), "Red");
         }
     }
 
@@ -394,7 +534,7 @@ namespace WpfApplication
             for (double t = 0; t <= 1; t += 0.5)
             {
                 Point finalPoint = Algorithm(t);
-                line.Algorithm(initialPoint, finalPoint);
+                line.Algorithm(initialPoint, finalPoint, "Red");
                 initialPoint = finalPoint;
             }
         }
@@ -425,23 +565,18 @@ namespace WpfApplication
         {
             bresenham = new Bresenham();
         }
-        public void Algorithm(List<Point> points)
+        public void Algorithm(List<Point> points, string color)
         {
             for (int i = 0; i < points.Count(); i++)
             {
-                bresenham.Algorithm(points[i % points.Count()], points[(i + 1) % points.Count()]);
+                bresenham.Algorithm(points[i % points.Count()], points[(i + 1) % points.Count()], color);
             }
-        }
-
-        public void DrawLine(Point p1, Point p2)
-        {
-            bresenham.Algorithm(p1, p2);
         }
     }
 
     public class Bresenham
     {
-        public void Algorithm(Point p1, Point p2)
+        public void Algorithm(Point p1, Point p2, string color)
         {
             int x1 = p1.X; int y1 = p1.Y;
             int x2 = p2.X; int y2 = p2.Y;
@@ -467,7 +602,7 @@ namespace WpfApplication
             int numerator = longest >> 1;
             for (int i = 0; i <= longest; i++)
             {
-                DrawPoints(x1, y1);
+                Draw(x1, y1, color);
                 numerator += shortest;
                 if (!(numerator < longest))
                 {
@@ -483,10 +618,10 @@ namespace WpfApplication
             }
         }
 
-        public void DrawPoints(int x, int y)
+        public void Draw(int x, int y, string color)
         {
             var windows = (MainWindow)Application.Current.MainWindow;
-            windows.PutPixel(x, y, "Red");
+            windows.PutPixel(x, y, color);
         }
     }
 
